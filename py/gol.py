f6a8d158ac4d1ff9fdefba2abad07ea624285830
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-09-02 01:06:59 +0000

    fixed issues from shallow list duplication using the old version of newBoard

--- py/gol.py
+++ py/gol.py
@@ -24,14 +24,24 @@
 
 def newBoard(rows, cols, sym='.'):#tested & works
   """a function to make a new board filled with symbol sym of row x col size """
-  return [[sym]*cols] * rows  
+  # need to use the more verbose method below because of shallow list in python
+  board = [[sym for i in range(cols)] for j in range(rows)]
+  return board
   
 def printBoard(board):
-  """A function to print a 2d array as a grid"""
-  for row in board:
-    for col in row:
-      print(col, end=' ')
+  """A function to print a 2d array (board) as a grid"""
+  for r in board:
+    for c in r:
+      print(c, end=' ') #end =' ' doube duties not adding a NL & spacing out
     print()
 
+def setCell(board, row, col, sym):
+  """A function that takes in 2d array "board" and sets the string value of a
+  cell at board[row][col] to symbol sym. """
+
+  board[row][col] = sym
+
+### test code
 tst = newBoard(10,5)
+setCell(tst, 1, 2, "X")
 printBoard(tst)

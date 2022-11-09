f1cdd26978494c30427c121ab4ceaeb3af00305e
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-11-09 19:11:39 +0000

    started airplane.py - have bug with seating duos

--- py/project files/airplane.py
+++ py/project files/airplane.py
@@ -1,72 +1,33 @@
-"""This program simulates the sales of tickets for a specific flight.
+"""
+This version of the plane filling program looks to keep groups togather based on a first come, first serve model
+Economy plus has been removed, with the underling idea
+that we would have regular economy and a discount for later fill in.
 
-A plane is represented by a list. Each element of the list is a row in
-the plane (with plane[0] being the front) and reach row is also a
-list.
+Assumptions - to stay in line with the instructor provided
+version - we will assume the plane can fit less then 100
+people, and no more than 3 are in a group
 
-Seats can be purchased as economy_plus or regular economy.
-
-Economy_plus passengers select their seats when they purchase their tickts.
-Economy passengers are assigned randomly when the flight is closer to full
-
-create_plane(rows,cols):
-  Creates and returns a plane of size rowsxcols
-  Plans have windows but no aisles (to keep the simulation simple)
-
-get_number_economy_sold(economy_sold):
-    Input: a dicitonary containing the number of regular economy seats sold. 
-           the keys are the names for the tickets and the values are how many
-
-    ex: {'Robinson':3, 'Lee':2 } // The Robinson family reserved 3
-    seats, the Lee family 2
-
-    Returns: the total number of seats sold
-
-get_avail_seats(plane,economy_sold):
-    Parameters: plane : a list of lists representing plaine
-    economy_sold : a dictionary of the economy seats sold but
-                   not necessarily assigned
-
-    Returns: the number of unsold seats
-
-    Notes: this loops over the plane and counts the number of seats
-           that are "avail" or "win" and removes the number of
-           economy_sold seats
-
-get_total_seats(plane):
-    Params: plane : a list of lists representing a plane
-    Returns: The total number of seats in the plane
-
-get_plane_string(plane):
-    Params: plane : a list of lists representing a plane
-    Returns: a string suitable for printing. 
-
-purchase_economy_plus(plane,economy_sold,name):
-    Params: plane - a list of lists representing a plane
-
-            economy_sold - a dictionary representing the economy
-                           sold but not assigned
-            name - the name of the person purchasing the seat
-
-    This routine randomly selects a seat for a person purchasing
-    economy_plus. Preference is given to window and front seats.
-
-seat_economy(plane,economy_sold,name):
-    Similar to purchase_economy_plus but just randomy assigns
-    a random seat.
-
-purchase_economy_block(plane,economy_sold,number,name):
-    Purchase regular economy seats. As long as there are sufficient seats
-    available, store the name and number of seats purchased in the
-    economy_sold dictionary and return the new dictionary
-
-
-fill_plane(plane):
-  takes an empty plane and runs our simulation to sell seats and then
-  seat the economy passengers. See comments in the function for details. 
-
-main():
-  The main driver program - start here
+General Algorithm -
+1. Build plane
+2. Get total number of seats
+3. Until seats full -
+   A. Get next customer or customer group, add to seating order
+   B. If too large to seat, pass
+4. Seat customers - 
+   A. sub 1 - as no priority is given to location, only trying
+   to seat groups, groups of 3 in order then groups of
+   2, seat groups from left to right, starting with 
+   groups in order of "booked" with the
+   groups first then fill with individals.
+   A. sub 2 - Check for side by side seats for groups of 2 before
+   moving on to unpaired in case of wierd scenes where we have a 
+   number of groups of 2 that need to be split
+   B. Most groups should be able to be sat in groups. 
+   If not, then randomly seat them (no further chunking
+   of note should be available now)
+   C. Fill in remaining seats with individuals
+5. Return filled plane list. (Also return number of failed bookings?)
+      
 
 """
 import random
@@ -278,11 +239,162 @@ def fill_plane(plane):
 
     return plane
     
+
+
+
+
+def group_size():
+  # assuming ~ 50% solo, ~25% pairs, ~25% triplets
+  size = random.randrange(4)
+  if size == 0:
+    size = 2 #test cost
+    # size = 1
+  return size
+
+def number_aisles(plane):
+  asiles = 0
+  for i in plane:
+    asiles = asiles + 1
+  return asiles
+
+def free_in_aisle(plane, aisle):
+  free = 0
+  for i in plane[aisle]:
+    if i == "win" or i == "avail":
+      free = free + 1
+  return free
+
+
+def fill_first_free(plane, aisle, fill):
+  for seat in range(len(plane[aisle])):
+    filled = False
+    if plane[aisle][seat] == 'avail' or plane[aisle][seat] == 'win':
+      plane[aisle][seat] = fill
+      break
+      
+def groupSeat(groupNum, type='S'):
+  '''
+  Assumes we need less than 100 total seating "groups"
+  '''
+  if groupNum < 10:
+    return type + "0" + str(groupNum) 
+  else:
+    return type + str(groupNum)
     
+
+def fill_plane2(plane):
+  unsold = get_total_seats(plane) #keep booking until full
+  missed_customers = 0 # to see how many we miss
+  groups = [] # list of bookings in order
+
+  asiles = number_aisles(plane) # for some short cuts
+  
+  while unsold > 0:
+    nextgroup = group_size()
+    if nextgroup <= unsold:
+      unsold = unsold - nextgroup
+      groups.append(nextgroup)
+    else:
+      missed_customers = missed_customers + nextgroup
+
+  groups_of_3 = [] # indexes of groups of 3 in order
+  groups_of_2 = [] # indexes of groups of 2 in order
+  solos = [] #indexes of solos in order.
+
+
+  
+  #find our groupings
+  for i in range(len(groups)): 
+    if groups[i] == 1:
+      solos.append(i)
+    elif groups[i] == 2:
+      groups_of_2.append(i)
+    else:
+      groups_of_3.append(i)
+
+  print('Number of passengers: ' + str(sum(groups)))
+  print('indexes of solos:')
+  print(solos)
+  print('indexes of pairs:')
+  print(groups_of_2)
+  print('indexes of trios:')
+  print(groups_of_3)
+  # seat groups of 3
+  for i in range(len(groups_of_3)):
+    #generate name for group to show off how fancy we are
+    name = groupSeat(groups_of_3[i], 'T')
     
+    # do easy ones first - full in a row
+    if i < asiles:
+
+      #seating first, so we can always use indexes 0,1,2
+      plane[i][0] = name
+      plane[i][1] = name
+      plane[i][2] = name
+
+    # fill in the remaineder of these
+    else:
+      to_be_seated = 3
+      for aisle in range(len(plane)):
+        free = free_in_aisle(plane, aisle)
+        while to_be_seated > 0 and free > 0:
+          
+          fill_first_free(plane, aisle, name)
+          free = free - 1
+          to_be_seated = to_be_seated -1
+          
+  # move on to groups of 2:           
+  pairs = 0 # get number of pairs of seats
+  for aisle in range(len(plane)):
+    free_pairs = (free_in_aisle(plane, aisle)) // 2 # int division
+    print('free pairs '+ str(free_pairs))
+    pairs = pairs + free_pairs # update number of pairs of seats
+
+  # now that we have the number of pairs, we can seat in pairs
+  # as long as they still exist then seat split
+  for i in range(len(groups_of_2)):
+    name = groupSeat(groups_of_2[i], 'P')
+    seated = False
+    
+    if pairs > 0: #fill the pairs first
+      for aisle in range(len(plane)):
+        if free_in_aisle(plane, aisle) > 2 and seated == False:
+          fill_first_free(plane, aisle, name)
+          fill_first_free(plane, aisle, name)
+          pairs = pairs - 1
+          seated = True
+
+    elif seated == False: #fill remaining blocks
+      print('elif fired off')
+      to_be_seated = 2
+      for aisle in range(len(plane)):
+        free = free_in_aisle(plane, aisle)
+        while to_be_seated > 0 and free > 0:
+          
+          fill_first_free(plane, aisle, name)
+          free = free - 1
+          to_be_seated = to_be_seated -1    
+          seated = True
+    else:
+      continue
+  
+  for i in range(len(solos)):
+    name = groupSeat(solos[i], 'S')
+    seated = False
+    for aisle in range(len(plane)):
+      if free_in_aisle(plane, aisle) and seated == False:
+        fill_first_free(plane, aisle, name)
+        seated = True
+   
+          
+  return plane
+  
+
 def main():
-    plane = create_plane(10,5)
-    plane = fill_plane(plane)
-    print(get_plane_string(plane))
+    length = 10
+    width = 5
+    plane = create_plane(length,width)
+    print(get_plane_string(fill_plane2(plane)))
+
 if __name__=="__main__":
     main()
